bc2a6ae00d482be28651b9ed0a243db5eee2a646
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-08-16 23:46:11 -0300

    resolución del desadío 32

--- EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
@@ -1,14 +1,12 @@
 package org.ar.miguel;
 
+import com.sun.javafx.image.IntPixelGetter;
 import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
 
 import java.lang.reflect.Array;
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 class Detalle {
     private int repeticion;
@@ -38,7 +36,47 @@ class Detalle {
 
 public class Main {
     public static void main(String[] args) {
-        e27();
+        e32();
+    }
+
+    static void e32() {
+        /*
+         * Dado un listado de números, encuentra el SEGUNDO más grande.
+         */
+        List<Integer> list = new ArrayList<>();
+
+        Scanner s = new Scanner(System.in);
+        int t = 0;
+        System.out.println("cuantos numeros vas a ingresar?");
+        t = Integer.parseInt(s.nextLine());
+
+        for (int i = 0; i < t; i++) {
+            list.add(Integer.parseInt(s.nextLine()));
+        }
+
+        int max = list.get(0);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+        }
+        int min = max;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < min) {
+                min = list.get(i);
+            }
+        }
+        int smax = min;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > smax && list.get(i) < max) {
+                smax = list.get(i);
+            }
+        }
+
+        System.out.println("el segundo máximo es : " + smax);
+        System.out.println("el máximo es : " + max);
+        System.out.println("el mínimo es : " + min);
+
     }
 
     static void e27() {
