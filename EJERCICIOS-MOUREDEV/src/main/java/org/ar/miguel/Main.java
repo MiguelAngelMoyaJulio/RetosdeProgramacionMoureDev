6a57cdb9c8ff58efa3517586b1e0076cbce521cc
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-08-23 21:07:01 -0300

    resolución desafío 34

--- EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
@@ -1,6 +1,7 @@
 package org.ar.miguel;
 
 import com.sun.javafx.image.IntPixelGetter;
+import com.sun.jmx.snmp.EnumRowStatus;
 import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
 
 import java.lang.reflect.Array;
@@ -66,7 +67,50 @@ class Detalle {
 
 public class Main {
     public static void main(String[] args) {
-        e15();
+        e34();
+    }
+
+
+    static void e34() {
+        /*
+         * Enunciado: Dado un array de enteros ordenado y sin repetidos,
+         * crea una función que calcule y retorne todos los que faltan entre
+         * el mayor y el menor.
+         * - Lanza un error si el array de entrada no es correcto.
+         */
+        Scanner s = new Scanner(System.in);
+        int[] numeros;
+        System.out.println("cuantos numeros vas a ingresar");
+        int lenNumeros = Integer.parseInt(s.nextLine());
+        numeros = new int[lenNumeros];
+
+        for (int i = 0; i < lenNumeros; i++) {
+            System.out.println("ingrese el numero");
+            numeros[i] = Integer.parseInt(s.nextLine());
+        }
+
+        int max = numeros[0];
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > max) {
+                max = numeros[i];
+            }
+        }
+
+        int min = numeros[0];
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < min) {
+                min = numeros[i];
+            }
+        }
+
+        System.out.println("máximo " + max);
+        System.out.println("mínimo " + min);
+        System.out.println("numeros restantes");
+        for (int i = min+1; i < max; i++) {
+            System.out.println("numero " + i);
+        }
     }
 
     static void e32() {
@@ -550,7 +594,7 @@ public class Main {
             cantidadDiasMenor = diasTranscurridos(fechaMenor, "menor");
             ma = Integer.parseInt(f1.substring(6, 10));
             me = Integer.parseInt(f2.substring(6, 10));
-            System.out.println("cantidad de dias : "+(cantidadDiasMayor+cantidadDiasMenor+365*(ma-me-1)));
+            System.out.println("cantidad de dias : " + (cantidadDiasMayor + cantidadDiasMenor + 365 * (ma - me - 1)));
         } else if (Integer.parseInt(f2.substring(6, 10)) > Integer.parseInt(f1.substring(6, 10))) {
             fechaMayor = f2;
             fechaMenor = f1;
@@ -558,7 +602,7 @@ public class Main {
             cantidadDiasMenor = diasTranscurridos(fechaMenor, "menor");
             ma = Integer.parseInt(f1.substring(6, 10));
             me = Integer.parseInt(f2.substring(6, 10));
-            System.out.println("cantidad de dias : "+(cantidadDiasMayor+cantidadDiasMenor+365*(ma-me-1))*-1);
+            System.out.println("cantidad de dias : " + (cantidadDiasMayor + cantidadDiasMenor + 365 * (ma - me - 1)) * -1);
         } else if (Integer.parseInt(f1.substring(3, 5)) > Integer.parseInt(f2.substring(3, 5))) {
             fechaMayor = f1;
             fechaMenor = f2;
