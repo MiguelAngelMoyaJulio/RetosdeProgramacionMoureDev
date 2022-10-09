6c9173608f386c6c6a387644623384e670ac7595
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 19:29:46 -0300

    RETO #6 REVIEW

--- EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
@@ -1052,26 +1052,4 @@ public class Main {
         }
     }
 
-    static void e6() {
-        /*
-         * Crea un programa que invierta el orden de una cadena de texto
-         * sin usar funciones propias del lenguaje que lo hagan de forma automática.
-         * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
-         */
-        Scanner s = new Scanner(System.in);
-        String f = "";
-        StringBuilder sb = new StringBuilder("");
-        System.out.println("ingrese la palabra");
-        String p = s.nextLine();
-        String[] ar = new String[p.length()];
-        for (int i = 0; i < p.length(); i++) {
-            ar[i] = p.substring(i, i + 1);
-        }
-        for (int i = ar.length - 1; i >= 0; i--) {
-            sb.append(ar[i]);
-        }
-        System.out.println(sb.toString());
-
-    }
-
 }
