1271d7f7a01abb3d10df6ded992c844a503deec4
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 19:25:10 -0300

    RETO #2 REVIEW

--- EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
@@ -1146,35 +1146,4 @@ public class Main {
 //        }
     }
 
-    static void e2() {
-        /*
-         * Escribe un programa que imprima los 50 primeros números de la sucesión
-         * de Fibonacci empezando en 0.
-         * - La serie Fibonacci se compone por una sucesión de números en
-         *   la que el siguiente siempre es la suma de los dos anteriores.
-         *   0, 1, 1, 2, 3, 5, 8, 13...
-         */
-        Scanner s = new Scanner(System.in);
-        System.out.println("ingrese la cantidad de elementos que desea ver");
-        int n = Integer.parseInt(s.nextLine());
-        int i = 1;
-        int ant2 = 0;
-        int ant = 0;
-        int nu = 0;
-        while (i <= n) {
-            if (i == 1) {
-                System.out.println(0);
-            } else if (i == 2 || i == 3) {
-                ant2 = 1;
-                System.out.println(1);
-                ant = ant2;
-            } else {
-                nu = ant2 + ant;
-                System.out.println(nu);
-                ant2 = ant;
-                ant = nu;
-            }
-            i++;
-        }
-    }
 }
