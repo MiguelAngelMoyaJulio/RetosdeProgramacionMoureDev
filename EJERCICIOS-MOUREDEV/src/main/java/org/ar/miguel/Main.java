2c18c30b1d152a87dbc76799926344297046887c
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 19:30:56 -0300

    RETO #7 REVIEW

--- EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
@@ -1029,27 +1029,4 @@ public class Main {
         System.out.println("numero binario : " + sb.reverse());
     }
 
-    static void e7() {
-        Scanner s = new Scanner(System.in);
-        String p = "";
-        String[] listado = new String[0];
-        List<String> listUnicos = new ArrayList<>();
-        System.out.println("ingrese un frase");
-
-        p = s.nextLine();
-        System.out.println("frase completa : ");
-        System.out.println(p);
-
-        listado = p.split(" ");
-        for (int i = 0; i < listado.length; i++) {
-            int t = 0;
-            for (int j = 0; j < listado.length; j++) {
-                if (listado[i].equalsIgnoreCase(listado[j])) {
-                    t++;
-                }
-            }
-            System.out.println("palabra " + listado[i] + " cantidad de veces que aparece " + t);
-        }
-    }
-
 }
