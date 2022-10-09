33fc3228d78473bf1965563b749638ef55ca8a33
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 19:20:45 -0300

    RETO #1 REVIEW

--- EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
@@ -214,7 +214,6 @@ public class Main {
             System.out.println("Tipo defensa : " + defensa);
             System.out.println("daño causado : " + df.format(daño));
         }
-
     }
 
     static void e34() {
@@ -1177,44 +1176,6 @@ public class Main {
             }
             i++;
         }
-    }
-
-    static void e1() {
-        /*
-         * Escribe una función que reciba dos palabras (String) y retorne
-         * verdadero o falso (Bool) según sean o no anagramas.
-         * - Un Anagrama consiste en formar una palabra reordenando TODAS
-         *   las letras de otra palabra inicial.
-         * - NO hace falta comprobar que ambas palabras existan.
-         * - Dos palabras exactamente iguales no son anagrama.
-         */
-        int coincidencias = 0;
-        Scanner s = new Scanner(System.in);
-        System.out.println("ingrese una palabra");
-        String p1 = s.nextLine();
-        System.out.println("ingrese otra palabra");
-        String p2 = s.nextLine();
-        p1.trim();
-        p2.trim();
-        if (p1.length() == p2.length()) {
-            if (p1.equals(p2)) {
-                System.out.println("las palabras ingresadas no forman son anagrama");
-            } else {
-                for (int i = 0; i < p1.length(); i++) {
-                    for (int j = 0; j < p2.length(); j++) {
-                        if (p1.substring(i, i + 1).equals(p2.substring(j, j + 1))) {
-                            coincidencias++;
-                        }
-                    }
-                }
-                if (coincidencias == p1.length()) {
-                    System.out.println("la palabra inicial es anagrama de la segunda");
-                }
-            }
-        } else {
-            System.out.println("las palabras ingresadas no son anagrama");
-        }
-
     }
 
     static void e0() {
