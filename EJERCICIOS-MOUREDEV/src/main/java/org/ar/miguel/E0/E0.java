8afaed31681863b50d870355ca726d6759f06532
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 19:23:17 -0300

    RETO #0

---
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/E0/E0.java
@@ -0,0 +1,26 @@
+package org.ar.miguel.E0;
+
+import java.util.Scanner;
+
+public class E0 {
+    public static void main(String[] args) {
+        /*
+         * Escribe un programa que muestre por consola (con un print) los
+         * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+         * cada impresión), sustituyendo los siguientes:
+         * - Múltiplos de 3 por la palabra "fizz".
+         * - Múltiplos de 5 por la palabra "buzz".
+         * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+         */
+        for (int i = 0; i < 100; i++) {
+            if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) {
+                System.out.println((i + 1) + " fizzbuzz");
+            } else if ((i + 1) % 5 == 0) {
+                System.out.println((i + 1) + " buzz");
+            } else if ((i + 1) % 3 == 0) {
+                System.out.println((i + 1) + " fizz");
+            }
+        }
+
+    }
+}
--- EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
+++ EJERCICIOS-MOUREDEV/src/main/java/org/ar/miguel/Main.java
@@ -1177,24 +1177,4 @@ public class Main {
             i++;
         }
     }
-
-    static void e0() {
-        /*
-         * Escribe un programa que muestre por consola (con un print) los
-         * números de 1 a 100 (ambos incluidos y con un salto de línea entre
-         * cada impresión), sustituyendo los siguientes:
-         * - Múltiplos de 3 por la palabra "fizz".
-         * - Múltiplos de 5 por la palabra "buzz".
-         * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
-         */
-        for (int i = 0; i < 100; i++) {
-            if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) {
-                System.out.println((i + 1) + " fizzbuzz");
-            } else if ((i + 1) % 5 == 0) {
-                System.out.println((i + 1) + " buzz");
-            } else if ((i + 1) % 3 == 0) {
-                System.out.println((i + 1) + " fizz");
-            }
-        }
-    }
 }
